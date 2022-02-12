2c490aa4e292ece99804ef3ff1829643c10c180a
digitalNimbusLabs <89986670+digitalNimbusLabs@users.noreply.github.com>
2022-02-12 13:39:22 -0600

    Update release-upload.sh

--- release-upload.sh
+++ release-upload.sh
@@ -2,6 +2,6 @@
 
 curl -H "Authorization: token $1" -H 'Accept: application/vnd.github.v3.raw' -L -O $2 
 
-curl -F 'payload_json={"content": "A new release is available"}' -F "file1=@$4.zip" $3
+curl -F 'payload_json={"content": "@here A new release is available"}' -F "file1=@$4.zip" $3
 
 
